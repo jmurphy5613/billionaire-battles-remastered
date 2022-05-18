8be4a9a372c4dac1be6ab14cf0f83771c2e20df1
jmurphy5613 <jmurphy5613@gmail.com>
2022-05-18 13:00:34 -0400

    fully set up data fetching, and passing props

--- client/components/boss-grid/BossGrid.tsx
+++ client/components/boss-grid/BossGrid.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import BossGridItem from "./BossGridItem";
 
 import { BillionaireBattlesAddress } from "../../helpers/addresses";
 import BillionaireBattles from '../../../server/artifacts/contracts/BillionaireBattles.sol/BillionaireBattles.json'
@@ -15,38 +16,41 @@ declare var window:any;
 const BossGrid = () => {
 
     const [id, setId] = useState(0);
+    const [bosses, setBosses] = useState<any>([]);
     const [dataFetched, setDataFetched] = useState(false);
 
-    useEffect(() => {
 
-        const fetchData = async () => {
-            const etherConnection = window.ethereum;
+    const fetchBosses = async () => {
+        const etherConnection = window.ethereum;
 
-            if(etherConnection) {
-                const provider = new ethers.providers.Web3Provider(etherConnection);
-                const signer = provider.getSigner();
-                const contract = new ethers.Contract(BillionaireBattlesAddress, BillionaireBattles.abi, signer);
-                
-                const nftIndexesHex = await contract.getBossIds();
-                console.log(nftIndexesHex);
-                //convert the values to integer form
-                const nftIndexesInt = [];
-                if(nftIndexesHex.length > 0) {
-                    for(let i = 0; i < nftIndexesHex.length; i++) {
-                        nftIndexesInt.push(parseInt(nftIndexesHex[i], 16));
-                    }
-                    console.log(nftIndexesInt);
-                }
-                //fetch data for indexes found
-                for(let i = 0; i < nftIndexesInt.length; i++) {
-                    const current = await contract.getBossStringDataById(nftIndexesInt[i]);
-                    console.log(hexToInt( current[6]));
+        if(etherConnection) {
+            const provider = new ethers.providers.Web3Provider(etherConnection);
+            const signer = provider.getSigner();
+            const contract = new ethers.Contract(BillionaireBattlesAddress, BillionaireBattles.abi, signer);
+            
+            const nftIndexesHex = await contract.getBossIds();
+            //convert the values to integer form
+            const nftIndexesInt = [];
+            for(let i = 0; i < nftIndexesHex.length; i++) {
+                nftIndexesInt.push(parseInt(nftIndexesHex[i], 16));
+            }
 
+            //fetch data for indexes found
+            for(let i = 0; i < nftIndexesInt.length; i++) {
+                const current = await contract.getBossStringDataById(nftIndexesInt[i]);
+                console.log(current, i);
+                //check if the current index has a valid boss
+                //current[1] is the name
+                if(!current[1] == "") {
+                    bosses[i] = (current);
                 }
             }
-            setDataFetched(true);
         }
-        fetchData();
+        setDataFetched(true);
+    }
+
+    useEffect(() => {
+        fetchBosses();
     }, [])
 
     return (
@@ -67,6 +71,26 @@ const BossGrid = () => {
             }}>
                 <ChevronLeftIcon htmlColor="#ffffff" fontSize="large" />
             </div>
+            {/*
+            Object Format:
+            0. wallet
+            1. name
+            2. description
+            3. image
+            4. health
+            5. maxHealth
+            6. attack
+            
+            */}
+            { dataFetched && <BossGridItem 
+                key={id} 
+                name={bosses[id][0]} 
+                health={hexToInt(bosses[id][4])} 
+                maxHealth={hexToInt(bosses[id][5])} 
+                description={bosses[id][2]} 
+                image={bosses[id][3]} 
+                wallet={bosses[id][0]}
+            />}
         </div>
     )
 }
--- client/components/boss-grid/BossGridItem.tsx
+++ client/components/boss-grid/BossGridItem.tsx
@@ -5,12 +5,25 @@ interface bossGridItemProps {
     health: number,
     maxHealth: number,
     description: string,
-    image: string
+    image: string,
+    wallet: string
 }
 
-const BossGridItem:React.FC<bossGridItemProps> = () => {
+const BossGridItem:React.FC<bossGridItemProps> = ({
+    name,
+    health,
+    maxHealth,
+    description,
+    image,
+    wallet
+}) => {
     return (
-        <div>
+        <div style={{
+            height: '450px',
+            width: '60%',
+            display: 'flex',
+            border: '1px solid white'
+        }}>
 
         </div>
     )
